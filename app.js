c77dbea89285d35a6c2234c678f31da44e9358b6
Wilbur125 <matt.s.wilbur@gmail.com>
2020-12-13 12:58:00 -0800

    node app.js runs, but troubleshooting the prompt user and handle role functions

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+const employees = [];
 
 function promptUser() {
 inquirer.prompt([
@@ -70,50 +69,39 @@ function promptIntern() {
   ])
 }
 
-async function handleRole() {
-    let selectedRole = await promptUser();
-    switch (promptUser.role) {
+ function handleRole() {
+    let selectedRole = promptUser();
+    switch (selectedRole.role) {
       case "Manager": 
-        let managerDetails = await promptManager();
-        return new Manager(promptUser.name, promptUser.id, promptUser.email, managerDetails.office);
+        let managerDetails = promptManager();
+        return new Manager(selectedRole.name, selectedRole.id, selectedRole.email, managerDetails.office);
 
       case "Engineer":
-        let engineerDetails = await promptEngineer();
-        return new Engineer(promptUser.name, promptUser.id, promptUser.email, engineerDetails.github);
+        let engineerDetails = promptEngineer();
+        return new Engineer(selectedRole.name, selectedRole.id, selectedRole.email, engineerDetails.github);
 
       case "Intern":
-        let internDetails = await promptIntern();
-        return new Intern(promptUser.name, promptUser.id, promptUser.email, internDetails.school);
+        let internDetails = promptIntern();
+        return new Intern(selectedRole.name, selectedRole.id, selectedRole.email, internDetails.school);
     }
 } 
 
-  .catch(error => {
-    if(error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else when wrong
-    }
-  });
+function writeToFile(data) {
+  fs.writeFile(
+      outputPath,
+      data,
+      (err) => {
+          if (err) throw err;
+          console.log("New HTML saved to Output folder");
+      }
+  );
+}
 
+async function startSurvey() {
+       await promptUser();
+       await handleRole();
+      const html = render(employees);
+      writeToFile(html);
+}
 
-
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+startSurvey();
